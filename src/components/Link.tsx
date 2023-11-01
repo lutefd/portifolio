dbb204450362f6f48def1192e68c28bfe89531c2
Luis Dourado <lfaddourado@gmail.com>
2023-11-01 10:28:43 -0300

    fix: partial fix of overflowing long repo names

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -14,7 +14,7 @@ const Link = ({ href, children }: LinkProps) => {
       href={href}
       target={isExternal ? '_blank' : '_self'}
       rel={isExternal ? 'noopener noreferrer' : ''}
-      className="underline decoration-neutral-600 underline-offset-4 transition-colors focus:decoration-neutral-500 focus:outline-offset-6 hover:decoration-neutral-500"
+      className="underline decoration-neutral-600 underline-offset-4 transition-colors focus:decoration-neutral-500 focus:outline-offset-6 hover:decoration-neutral-500 truncate"
     >
       {children}
     </a>
