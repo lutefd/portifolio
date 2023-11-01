2c91bf10bcb9ba6cd556e7361af82f42b8cab307
Luis Dourado <lfaddourado@gmail.com>
2023-11-01 00:12:21 -0300

    feat: configure posts schema

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'zod';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.string().transform((str) => new Date(str)),
+    isDraft: z.boolean().optional().default(false),
+  }),
+});
+
+export const collections = { blog };
