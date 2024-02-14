00d0577ca6b6d8df3b3beb48aa2c377a26aa9203
Luis Dourado <lfaddourado@gmail.com>
2024-02-14 00:03:33 -0300

    chore(style): update code style in config
    This commits update de quotes styling in the config code based on the prettier pattern I'm using

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
-  integrations: [
-    mdx(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+	prefetch: true,
+	integrations: [
+		mdx(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
 });
