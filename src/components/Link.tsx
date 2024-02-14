52749c7048594c52162e171d77174db0784b224f
Luis Dourado <lfaddourado@gmail.com>
2024-02-14 12:03:14 -0300

    refactor(ui): add dynamic className to component
    This commit adds a dynamic className so that it can be altered when the component is used for a specific cenario

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,24 +1,29 @@
-import { useMemo } from "react";
+import { cn } from '@/lib/utils';
+import { useMemo } from 'react';
 
 interface LinkProps {
-  href: string;
-  children: React.ReactNode;
+	href: string;
+	children: React.ReactNode;
+	className?: string;
 }
 
-const Link = ({ href, children }: LinkProps) => {
-  const isExternal = useMemo(() => {
-    return href.startsWith("http");
-  }, [href]);
-  return (
-    <a
-      href={href}
-      target={isExternal ? "_blank" : "_self"}
-      rel={isExternal ? "noopener noreferrer" : ""}
-      className="underline decoration-neutral-600 underline-offset-4 transition-colors focus:decoration-neutral-500 focus:outline-offset-6 hover:decoration-neutral-500 truncate"
-    >
-      {children}
-    </a>
-  );
+const Link = ({ href, children, className }: LinkProps) => {
+	const isExternal = useMemo(() => {
+		return href.startsWith('http');
+	}, [href]);
+	return (
+		<a
+			href={href}
+			target={isExternal ? '_blank' : '_self'}
+			rel={isExternal ? 'noopener noreferrer' : ''}
+			className={cn(
+				className,
+				'underline decoration-neutral-600 underline-offset-4 transition-colors focus:decoration-neutral-500 focus:outline-offset-6 hover:decoration-neutral-500 truncate'
+			)}
+		>
+			{children}
+		</a>
+	);
 };
 
 export default Link;
