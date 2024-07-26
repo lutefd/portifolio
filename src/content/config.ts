e475ff6df37fdbc06bd152d68e41b50523d25d2c
Luis Dourado <lfaddourado@gmail.com>
2024-07-26 20:13:55 -0300

    style(formatting): improve formatting

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,25 @@
-import { defineCollection } from 'astro:content';
-import { z } from 'zod';
+import { defineCollection } from "astro:content";
+import { z } from "zod";
 
 const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		formattedTitle: z.string().optional().default('⁖ lutefd'),
-		date: z.string().transform((str) => new Date(str)),
-		isDraft: z.boolean().optional().default(false),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    formattedTitle: z.string().optional().default("⁖ lutefd"),
+    date: z.string().transform((str) => new Date(str)),
+    isDraft: z.boolean().optional().default(false),
+  }),
 });
 const workBlog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		formattedTitle: z.string().optional().default('⁖ lutefd'),
-		date: z.string().transform((str) => new Date(str)),
-		isDraft: z.boolean().optional().default(false),
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    formattedTitle: z.string().optional().default("⁖ lutefd"),
+    date: z.string().transform((str) => new Date(str)),
+    isDraft: z.boolean().optional().default(false),
+  }),
 });
 
-export const collections = { blog: blog, 'work-blog': workBlog };
+export const collections = { blog: blog, "work-blog": workBlog };
