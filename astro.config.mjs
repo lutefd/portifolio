9c83183291ae1f9805e00e6d3eac25d6cb4bd4fc
Luis Dourado <lfaddourado@gmail.com>
2025-09-04 21:25:27 -0300

    feat: update deps and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,21 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://luisdourado.com",
-  prefetch: true,
-  redirects: {
-    "/feed": "/feed.xml",
-    "/rss": "/feed.xml",
-    "/rss.xml": "/feed.xml",
-  },
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    robotsTxt(),
-  ],
+	site: "https://luisdourado.com",
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	prefetch: true,
+	redirects: {
+		"/feed": "/feed.xml",
+		"/rss": "/feed.xml",
+		"/rss.xml": "/feed.xml",
+	},
+	integrations: [mdx(), sitemap(), react(), robotsTxt()],
 });
